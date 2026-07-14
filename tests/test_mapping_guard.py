a139dd20966f5df2417a5e6b7f2ad739cfb66c6e
Gumb-D <jjcui2k@gmail.com>
2026-07-14 21:26:41 +0800

    test: require TX Integrated identity for TX Mini RFS

--- tests/test_mapping_guard.py
+++ tests/test_mapping_guard.py
@@ -4,7 +4,7 @@ from scripts import mapping_guard
 
 
 class MilestoneMappingGuardTests(unittest.TestCase):
-    def _headers(self, width=130):
+    def _headers(self, width=180):
         return [["" for _ in range(width)] for _ in range(4)]
 
     def _set_column(self, headers, index, field_id, stage, task, display):
@@ -20,27 +20,35 @@ class MilestoneMappingGuardTests(unittest.TestCase):
             configured,
         )
 
-    def test_rebinds_tx_mini_rfs_from_planned_end_119_to_actual_end_121(self):
+    def test_rebinds_tx_mini_rfs_to_tx_integrated_actual_end_not_nearby_mrcf(self):
         headers = self._headers()
         self._set_column(
             headers,
             119,
-            "WP11400|AC0000111569|planned_end_date",
+            "WP11200|AC0000111568|plan_end_date",
+            "Hardware Dismantling",
+            "MRCF",
+            "planned end time",
+        )
+        self._set_column(
+            headers,
+            121,
+            "WP11200|AC0000111568|actual_end_date",
+            "Hardware Dismantling",
+            "MRCF",
+            "actual end time",
+        )
+        self._set_column(
+            headers,
+            146,
+            "WP11400|AC0000111569|plan_end_date",
             "Software Commissioning",
             "TX Integrated",
             "planned end time",
         )
         self._set_column(
             headers,
-            120,
-            "WP11400|AC0000111569|actual_start_date",
-            "Software Commissioning",
-            "TX Integrated",
-            "actual start time",
-        )
-        self._set_column(
-            headers,
-            121,
+            148,
             "WP11400|AC0000111569|actual_end_date",
             "Software Commissioning",
             "TX Integrated",
@@ -49,14 +57,28 @@ class MilestoneMappingGuardTests(unittest.TestCase):
 
         resolved = self._resolve(headers, {"RFS": 119})
 
-        self.assertEqual(resolved["RFS"], 121)
+        self.assertEqual(resolved["RFS"], 148)
 
-    def test_rejects_planned_end_when_no_actual_completion_column_exists(self):
+    def test_rejects_other_rfs_like_task_when_tx_integrated_is_missing(self):
         headers = self._headers()
         self._set_column(
             headers,
-            119,
-            "WP11400|AC0000111569|planned_end_date",
+            137,
+            "WP11400|OTHER|actual_end_date",
+            "Operation",
+            "Site Integrated",
+            "actual end time",
+        )
+
+        with self.assertRaises(mapping_guard.MappingValidationError):
+            self._resolve(headers, {"RFS": 119})
+
+    def test_rejects_planned_tx_integrated_when_no_actual_completion_exists(self):
+        headers = self._headers()
+        self._set_column(
+            headers,
+            146,
+            "WP11400|AC0000111569|plan_end_date",
             "Software Commissioning",
             "TX Integrated",
             "planned end time",
@@ -65,9 +87,9 @@ class MilestoneMappingGuardTests(unittest.TestCase):
         with self.assertRaises(mapping_guard.MappingValidationError):
             self._resolve(headers, {"RFS": 119})
 
-    def test_rejects_ambiguous_actual_completion_candidates(self):
+    def test_rejects_ambiguous_tx_integrated_actual_completion_candidates(self):
         headers = self._headers()
-        for index in (121, 125):
+        for index in (148, 152):
             self._set_column(
                 headers,
                 index,
@@ -80,20 +102,20 @@ class MilestoneMappingGuardTests(unittest.TestCase):
         with self.assertRaises(mapping_guard.MappingValidationError):
             self._resolve(headers, {"RFS": 119})
 
-    def test_keeps_a_valid_actual_completion_hint(self):
+    def test_keeps_a_valid_tx_integrated_actual_completion_hint(self):
         headers = self._headers()
         self._set_column(
             headers,
-            121,
+            148,
             "WP11400|AC0000111569|actual_end_date",
             "Software Commissioning",
             "TX Integrated",
             "actual end time",
         )
 
-        resolved = self._resolve(headers, {"RFS": 121})
+        resolved = self._resolve(headers, {"RFS": 148})
 
-        self.assertEqual(resolved["RFS"], 121)
+        self.assertEqual(resolved["RFS"], 148)
 
 
 if __name__ == "__main__":
